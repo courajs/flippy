6d83b37fcde8391bf73fbb5ab5dbc9d07d64b6b0
Aaron Sikes <aaron@sikes.io>
2021-11-12 10:19:15 -0500

    Better board init

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,11 @@ import {tracked} from '@glimmer/tracking';
 import {action} from '@ember/object';
 
 class Cell {
-  @tracked on = false;
+  @tracked on;
+
+  constructor(on = false) {
+    this.on = on;
+  }
 
   @action toggle() {
     this.on = !this.on;
@@ -17,16 +21,14 @@ class Board {
   rows;
   cols;
 
-  constructor(width, height) {
+  constructor(width, height, cells) {
     this.width = width;
     this.height = height;
     let len = width*height;
-    this.cells = new Array(len);
+    this.cells = cells;
+
     this.rows = new Array(height);
     this.cols = new Array(width);
-    for (let i = 0; i<len; i++) {
-      this.cells[i] = new Cell();
-    }
     for (let i = 0; i<this.rows.length; i++) {
       this.rows[i] = new Array(width);
     }
@@ -40,6 +42,23 @@ class Board {
         this.rows[row][col] = cell;
       }
     }
+  }
+
+  static blank(width, height) {
+    let len = width*height;
+    let cells = new Array(len);
+    for (let i=0; i<len; i++) {
+      cells[i] = new Cell(false);
+    }
+    return new Board(width, height, cells);
+  }
+  static random(width, height) {
+    let len = width*height;
+    let cells = new Array(len);
+    for (let i=0; i<len; i++) {
+      cells[i] = new Cell(Math.random() > 0.5);
+    }
+    return new Board(width, height, cells);
   }
 
   cell(x, y) {
@@ -59,5 +78,5 @@ class Board {
 }
 
 export default class extends Controller {
-  @tracked board = new Board(5, 5);
+  @tracked board = Board.blank(5, 5);
 }
